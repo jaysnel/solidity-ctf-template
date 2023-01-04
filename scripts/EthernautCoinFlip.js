e4eb00047156b7901175ad2d6f0a223f19501225
jaysnel <jaylan210@gmail.com>
2023-01-04 07:54:52 -0500

    not sure how this will working without contract but will see

---
+++ scripts/EthernautCoinFlip.js
@@ -0,0 +1,15 @@
+const main = async () => {
+
+} 
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+runMain();
