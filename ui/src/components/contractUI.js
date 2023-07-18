1594692adf49cbbf458f8d00c95976bb4cc7edb3
Jaylan Snelson <jaylan210@gmail.com>
2023-07-18 16:27:41 -0400

    Using ethers 5.7.2 since it seems +6.0.0 are unstable. good tempalte to have to input things for ui

--- ui/src/components/contractUI.js
+++ ui/src/components/contractUI.js
@@ -2,22 +2,25 @@ import React, { useEffect } from 'react'
 import contractAbi from '../utils/contractAbi.json'
 
 export default function ContractUI() {
-    const targetAddress = '0x39588a02aD9A2A302323D6aAC054C0E610E8775F';
+    const targetAddress = '';
     const { ethers} = require('ethers');
-    console.log('ethers.js: ', ethers);
+    const network = 'sepolia';
+    console.log('ethers: ', ethers)
     const getContractData = async () => {
         try {
             const { ethereum } = window;
             if(ethereum) {
-                const provider = new ethers.JsonRpcProvider(ethereum);
+                // const provider = new ethers.JsonRpcProvider(ethereum);
+                const provider = new ethers.providers.Web3Provider(ethereum)
                 await provider.send("eth_requestAccounts", []);
-                console.log('provider: ', provider)
-                // const signer = await provider.getSigner();
-                // const contract = new ethers.Contract(targetAddress, contractAbi, signer);
+                // console.log('provider: ', provider)
+                const signer = provider.getSigner();
+                // console.log('signer: ', signer);
+                const contract = new ethers.Contract(targetAddress, contractAbi, signer);
                 // console.log('Contract: ', contract);
             }
         } catch(err) {
-            console.log("FAILURE: ", err.message);
+            console.log("Request Failed: ", err.message);
         }
     }
 
